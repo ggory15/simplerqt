bdcc06de05ba548cb419a1810170de496c5fee80
saga0619 <june992@gmail.com>
2020-09-23 10:49:05 +0900

    Initial StartUp

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['talos_wbc_gui'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    requires=['roscpp'],
+    scripts=['scripts/talos_wbc_gui']
 )
 
 setup(**d)
